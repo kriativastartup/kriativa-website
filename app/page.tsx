748a04ad198ee197a3929b4366f97c0f6710a8f2
Kriativa <kriativa.development@gmail.com>
2026-01-30 01:17:19 +0100

    feat: add menu button with icon to navigation for improved accessibility

--- app/page.tsx
+++ app/page.tsx
@@ -65,6 +65,14 @@ export default function Home() {
                 className="px-4 pot:inline-flex hidden transition-all hover:bg-pallete-1/80 bg-pallete-1 text-white text-[14px] font-airblight font-semibold py-2.5 rounded-full">
                 Solicitar Orçamento
               </button>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                  stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                  className="icon size-7 text-pallete-1 icon-tabler icons-tabler-outline icon-tabler-menu">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                  <path d="M4 8l16 0" /><path d="M4 16l16 0" /></svg>
+              </button>
             </div>
           </nav>
         </motion.div>
